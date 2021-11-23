6eed41fee9643dcb4e01bc2aeb24bea01ad9fd90
m0renz0 <merveille.wol@gmail.com>
2021-11-23 00:53:10 -0500

    Added Game Over Screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{ 
+
+    public TextMeshProUGUI current;
+    public TextMeshProUGUI high;
+
+
+
+    // public void Awake()
+    // {
+    //     this.current.text = "Lines Cleared: ";
+    //     this.high.text = "High Score: ";
+    // }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Tetris");
+    }
+
+    public void PlayOriginal()
+    {
+        SceneManager.LoadScene("TetrisOriginal");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+}
